e0cde0af3c9fb82de855a491b332afc135893852
kovacsnandor <kovacsnandor@gmail.com>
2026-02-09 14:09:55 +0100

    axios, service, store, Generic table

---
+++ client/src/components/Table/ButtonsCrud.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="d-flex">
+    <ButtonsCrudDelete
+      class="ms-2"
+      :id="id"
+      @delete="$emit('delete', $event)"
+    />
+    <ButtonsCrudUpdate
+      class="ms-2"
+      :id="id"
+      @update="$emit('update', $event)"
+    />
+    <ButtonsCrudCreate class="ms-2" @create="$emit('create', $event)" />
+  </div>
+</template>
+
+<script>
+import ButtonsCrudCreate from "./ButtonsCrudCreate.vue";
+import ButtonsCrudDelete from "./ButtonsCrudDelete.vue";
+import ButtonsCrudUpdate from "./ButtonsCrudUpdate.vue";
+export default {
+  name: "ButtonsCrud",
+  components: {
+    ButtonsCrudCreate,
+    ButtonsCrudDelete,
+    ButtonsCrudUpdate,
+  },
+  props: {
+    id: { type: Number, required: true },
+  },
+};
+</script>
+
+<style></style>
